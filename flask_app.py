dae170de6c3dc26a64ac540c057a61169fb21eba
Dinuk002 <dinukoffcl@gmail.com>
2026-06-01 12:03:47 +0530

    new changes to the routes

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
@@ -10,10 +10,29 @@ def index():
 def add(number1,number2):
     return f'{number1+number2}'
 
+
+@app.route('/requests')
+def handle_requests():
+    return render_template('index.html')
+
+@app.route('/return' ,methods=['POST'])
+def retunr_data():
+    name=request.form['name']
+    email=request.form['email']
+    if name=="Dinuk" and email=="dinuk@example.com":
+        return f'Name: {name}, Email: {email}'
+    return "Invalid credentials"
+
+
 @app.route('/handle_url_params')
 def handle_params():
     greeting=request.args['greeting']
     name=request.args['name']
+    return f'{greeting},{name}'
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
