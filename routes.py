6a7057ec01315f45cad4f4d7a6b989c0df6a56ba
Dinuk002 <dinukoffcl@gmail.com>
2026-06-01 15:51:28 +0530

    Completed delete function

--- routes.py
+++ routes.py
@@ -1,10 +1,25 @@
-from flask import render_template,request
+from flask import render_template, request, redirect, url_for
 
 from models import User
 
 def register_routes(app,db):
 
-    @app.route('/')
+    @app.route('/', methods=['GET','POST'])
     def index():
-        users=User.query.all()
-        return render_template('index.html',users=users)
+        if request.method=='GET':
+            users=User.query.all()
+            return render_template('index.html',users=users)
+        elif request.method=='POST':
+            name=request.form['name']
+            email=request.form['email']
+
+            new_user=User(name=name,email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('index'))
+
+    @app.route('/delete/<int:user_id>', methods=['POST'])
+    def delete_user(user_id):
+        User.query.filter_by(id=user_id).delete()
+        db.session.commit()
+        return redirect(url_for('index'))
