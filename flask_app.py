4d05e8b91c81237b83d1374ae9d8a0f2f55a5a95
Dinuk002 <dinukoffcl@gmail.com>
2026-06-01 09:18:54 +0530

    Add recent changes

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello world !"
+
+@app.route('/greet/<int:number1>/<int:number2>')
+def add(number1,number2):
+    return f'{number1+number2}'
+
+@app.route('/handle_url_params')
+def handle_params():
+    greeting=request.args['greeting']
+    name=request.args['name']
 
 if __name__ == "__main__":
     app.run(debug=True)
